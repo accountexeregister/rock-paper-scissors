7fa0afff27896ff63632d6deee6f0c6fbba436bf
ubuntujunior <yeohchongyi2@outlook.com>
2022-09-14 21:14:45 +0800

    Refactor playRound function return values and create function game

--- js/script.js
+++ js/script.js
@@ -14,17 +14,21 @@ function getComputerChoice() {
     }
 }
 
+function standardiseInput(playerSelection) {
+    ////Convert playerSelection to new playerSelection with capitalised first character followed by lowercase characters
+    //Example: "rock" turns to "Rock"
+    return playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1).toLowerCase();
+}
 function playRound(playerSelection, computerSelection) {
     //playerSelection as parameter for user input string(either "rock", "paper" or "scissors" which are case-insensitive) 
     //computerSelection as parameter for getComputerChoice()
 
     //Convert playerSelection to new playerSelection with capitalised first character followed by lowercase characters
     //Example: "rock" turns to "Rock"
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1).toLowerCase();
-
-    //if playerSelection is not "Rock", "Paper" or "Scissors", return "invalid player selection" 
+    playerSelection = standardiseInput(playerSelection);
+    //if playerSelection is not "Rock", "Paper" or "Scissors", return undefined 
     if (playerSelection !== "Rock" && playerSelection !== "Paper" && playerSelection !== "Scissors") {
-        return "invalid player selection";
+        return undefined;
     }
     switch (true) {
         //if playerSelection = "rock" and computerSelection = "scissors",
@@ -33,16 +37,16 @@ function playRound(playerSelection, computerSelection) {
         case (playerSelection === "Paper" && computerSelection === "Rock"):
         //or playerSelection = "scissors" and computerSelection = "paper",
         case (playerSelection === "Scissors" && computerSelection === "Paper"):
-            //then return "You win! (playerSelection) beats (computerSelection)" 
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+            //then return true 
+            return true;
 
         //if playerSelection same as computerSelection, 
-        //return "It is a tie! (playerSelection) does not beat (computerSelection)"
+        //return undefined
         case (playerSelection === computerSelection):
-            return `It is a tie! ${playerSelection} does not beat ${computerSelection}`;
+            return undefined;
         default: 
-            //else return "You lose! (computerSelection) beats (playerSelection)"
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
+            //else return false
+            return false;
     }
     
     
@@ -50,4 +54,60 @@ function playRound(playerSelection, computerSelection) {
     
 }
 
-console.log(playRound("roCk", getComputerChoice()));
+//`You win! ${playerSelection} beats ${computerSelection}`
+//`No change in score!`
+//`You lose! ${computerSelection} beats ${playerSelection}`
+function game() {
+    //create variable playerScore and initialise it to 0
+    let playerScore = 0;
+    //create variable computerScore and initialise it to 0
+    let computerScore = 0;
+    //loop 5 times 
+    for (let i = 0; i < 5; i++) {
+        //in the loop, prompt user to enter input and store it in new variable playerInput
+        let playerInput = prompt("Enter your input");
+
+        //store getComputerChoice() in computerChoice variable
+        let computerChoice = getComputerChoice();
+
+        //in the loop, call playRound function with playerInput as argument for first parameter
+        //and getComputerChoice() as argument for second parameter
+        //and store the result in variable result
+        let result = playRound(playerInput, computerChoice);
+       
+        //if result is undefined, print "no change in score"
+        if (result === undefined) {
+            console.log("No change in score");
+        //if result is true, increment playerScore by 1 and print player win in console
+        } else if (result) {
+            playerScore++;
+            console.log(`You win! ${standardiseInput(playerInput)} beats ${computerChoice}`);
+        }
+        //else increment computerScore by 1 and print computer win in console
+        else {
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${standardiseInput(playerInput)}`);
+        }
+
+    }
+    
+    //outside the loop, if playerScore > computerScore, print player win in console
+    if (playerScore > computerScore) {
+        console.log(`You win! The final score is ${playerScore}:${computerScore}`);
+    //else if computerScore > playerScore, print computer win in console
+    } else if (computerScore > playerScore) {
+        console.log(`You lose! The final score is ${computerScore}:${playerScore}`);
+    //else print tie in console
+    } else {
+        console.log(`It is a tie! The final score is ${playerScore}:${computerScore}`);
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
+game();
